c287ef670a8f0c7122426f45b44146c8f773a6da
keyboardDrummer <rgv.willems@gmail.com>
2016-08-06 14:12:26 +0200

    Added new test project to replace weirdly broken old test project.

---
+++ DebtRatchet.Test/TestMethodParameterCountAnalyzer.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using DebtAnalyzer.MethodDebt;
+using TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace DebtAnalyzer.Test
+{
+	
+    public class TestMethodParameterCountAnalyzer : CodeFixVerifier
+	{
+		public TestMethodParameterCountAnalyzer()
+		{
+			MethodParameterCountAnalyzer.DefaultMaximumParameterCount = 5;
+		}
+
+		[Test]
+		public void TestFix()
+		{
+			VerifyCSharpFix(TestProgramInput, FixedProgram, allowNewCompilerDiagnostics: true);
+		}
+
+		//No diagnostics expected to show up
+		[Test]
+        public void TestEmptyProgramHasNoDiagnostics()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+		[Test]
+		public void TestDiagnostic()
+		{
+			var test = TestProgramInput;
+			var expected = new DiagnosticResult
+			{
+				Id = "MaxParameterCount",
+				Message = String.Format("Method MyBadMethod2443 has 6 parameters while it should not have more than 4."),
+				Severity = DiagnosticSeverity.Info,
+				Locations =
+					new[] {
+							new DiagnosticResultLocation("Test0.cs", 17, 14)
+						}
+			};
+
+			VerifyCSharpDiagnostic(new[] { test, DebtAnalyzerTestUtil.DebtMethodAnnotation, MaxParametersAnnotation }, expected);
+		}
+
+		[Test]
+        public void TestDiagnosticAsError()
+        {
+            var test = TestProgramInput;
+            var expected = new DiagnosticResult
+            {
+                Id = "MaxParameterCount",
+                Message = "Method MyBadMethod2443 has 6 parameters while it should not have more than 5.",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 17, 14)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(new [] { test, DebtAnalyzerTestUtil.DebtMethodAnnotation, DebtAnalyzerTestUtil.DebtAsError }, expected);
+        }
+
+		[Test]
+		public void TestDiagnosticAnnotation()
+		{
+			VerifyCSharpDiagnostic(new[] {DebtAnalyzerTestUtil.DebtMethodAnnotation, FixedProgram });
+		}
+		
+		public static string MaxParametersAnnotation => @"
+
+using System;
+using DebtAnalyzer;
+
+[assembly: MaxParameters(4)]
+namespace DebtAnalyzer
+{
+	[AttributeUsage(AttributeTargets.Assembly)]
+	class MaxParameters : Attribute
+	{
+		public MaxParameters(int parameterCount)
+		{
+			ParameterCount = parameterCount;
+		}
+
+		public int ParameterCount { get; }
+	}
+}";
+
+		public static string FixedProgram => @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using DebtAnalyzer;
+
+namespace ConsoleApplication1
+{
+    class TypeName
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [DebtMethod(LineCount = 1, ParameterCount = 6)]
+        void MyBadMethod2443(int a, int b, int c, int d, int e, int g)
+        {
+
+        }
+    }
+}
+";
+
+		public static string TestProgramInput => @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using DebtAnalyzer;
+
+namespace ConsoleApplication1
+{
+    class TypeName
+    {   
+        /// <summary>
+        /// 
+        /// </summary>
+        void MyBadMethod2443(int a, int b, int c, int d, int e, int g)
+        {
+
+        }
+    }
+}
+";
+
+		[Test]
+		public void TestConstructorFix()
+		{
+			VerifyCSharpFix(WithConstructor, FixedWithConstructor, allowNewCompilerDiagnostics: true);
+		}
+
+		public static string FixedWithConstructor => @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using DebtAnalyzer;
+
+namespace ConsoleApplication1
+{
+    class TypeName
+    {
+        [DebtMethod(LineCount = 1, ParameterCount = 6)]
+        TypeName(int a, int b, int c, int d, int e, int g) {
+        }
+    }
+}
+";
+
+		public static string WithConstructor => @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using DebtAnalyzer;
+
+namespace ConsoleApplication1
+{
+    class TypeName
+    {   
+        TypeName(int a, int b, int c, int d, int e, int g) {
+        }
+    }
+}
+";
+		protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new MethodDebtAnnotationProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new MethodDebtAnalyzer();
+        }
+    }
+}
---
+++ DebtRatchet.Test/TestTypeLengthAnalyzer.cs
@@ -0,0 +1,125 @@
+﻿using DebtAnalyzer.ClassDebt;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TestHelper;
+
+namespace DebtAnalyzer.Test
+{
+	public class TestTypeLengthAnalyzer : CodeFixVerifier
+	{
+		public TestTypeLengthAnalyzer()
+		{
+			TypeLengthAnalyzer.DefaultMaximumTypeLength = 10;
+		}
+
+		static string GeneratedType => @"
+namespace ConsoleApplication1
+{
+	[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+	public class ResCommon
+	{
+        int a;
+
+
+
+
+
+
+
+
+
+
+
+
+        int b;
+    }
+}";
+		static string LongType => @"
+using System;
+
+namespace ConsoleApplication1
+{
+    class LongMethodClass
+    {
+        int a;
+
+
+
+
+
+
+
+
+
+        class InnerClass { }
+    }
+}";
+
+		static string LongTypeFixed => @"
+using System;
+using DebtAnalyzer;
+
+namespace ConsoleApplication1
+{
+    [DebtType(LineCount = 14, FieldCount = 1)]
+    class LongMethodClass
+    {
+        int a;
+
+
+
+
+
+
+
+
+
+        class InnerClass { }
+    }
+}";
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			return new TypeDebtAnalyzer();
+		}
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider()
+		{
+			return new TypeDebtAnnotationProvider();
+		}
+
+		[Test]
+		public void TestDiagnostic()
+		{
+			var test = LongType;
+			var expected = new DiagnosticResult
+			{
+				Id = "TypeLengthAnalyzer",
+				Message = "Type LongMethodClass is 14 lines long while it should not be longer than 10 lines.",
+				Severity = DiagnosticSeverity.Info,
+				Locations =
+					new[]
+					{
+						new DiagnosticResultLocation("Test0.cs", 6, 11)
+					}
+			};
+
+			VerifyCSharpDiagnostic(test, expected);
+		}
+
+		[Test]
+		public void TestNoErrorOnGeneratedCode()
+		{
+			VerifyCSharpDiagnostic(GeneratedType);
+		}
+
+		[Test]
+		public void TestFix()
+		{
+			VerifyCSharpFix(LongType, LongTypeFixed, allowNewCompilerDiagnostics: true);
+		}
+	}
+}

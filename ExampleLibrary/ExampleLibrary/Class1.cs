c2a6ae56bb00db25f4f5ab32d03af0528adf11ec
keyboardDrummer <rgv.willems@gmail.com>
2018-01-07 17:47:32 +0100

    Some improvements

---
+++ ExampleLibrary/ExampleLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using DebtRatchet;
+using ExampleLibrary;
+using System;
+
+[assembly: MaxMethodLength(2)]
+
+namespace ExampleLibrary
+{   
+
+#pragma warning disable CS0168 // Variable is declared but never used
+    public class Class1
+    {
+        public void TooLongMethod()
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+        }
+    }
+#pragma warning restore CS0168 // Variable is declared but never used
+}

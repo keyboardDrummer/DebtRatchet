336e3c3e7d63865767ae5e5e3105ae849836f526
keyboardDrummer <rgv.willems@gmail.com>
2018-01-07 20:43:17 +0100

    Updater and statistics finally working

--- StatisticsProvider/Program.cs
+++ StatisticsProvider/Program.cs
@@ -23,7 +23,7 @@ namespace StatisticsProvider
 			else
 			{
 				var onlyNumbers = args.Contains("-n");
-				ProvideStatistics(args[0], onlyNumbers, GetEmptyStatistics(args));
+                ProvideStatistics(args[0], onlyNumbers, GetEmptyStatistics(args)).GetAwaiter().GetResult();
 			}
 		}
 
@@ -37,7 +37,7 @@ namespace StatisticsProvider
 			return new Statistics(typeStatistics, methodStatistics);
 		}
 
-		static async void ProvideStatistics(string solutionPath, bool onlyNumbers, Statistics statistics)
+		static async Task ProvideStatistics(string solutionPath, bool onlyNumbers, Statistics statistics)
 		{
 			try
 			{
@@ -58,6 +58,7 @@ namespace StatisticsProvider
             {
                 Console.Write(e.Message);
             }
+            Console.WriteLine("Done");
         }
 
 		static async Task<Solution> GetSolution(string path)
